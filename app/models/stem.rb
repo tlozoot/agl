44ae34b83ff15d4bbbe3fa7cfa409ed6e5457513
Jonathan Levine <jonathan@jonathan-levines-macbook-pro.local>
2010-08-26 12:16:25 -0400

    add experiment type to stems

--- app/models/stem.rb
+++ app/models/stem.rb
@@ -3,7 +3,7 @@ class Stem < ActiveRecord::Base
   has_many :results
   has_many :participants, :through => :results
   
-  attr_accessor :clipart
+  attr_accessor :clipart, :experiment_type
   
   def self.assign_pictures_to_stems_of_type(type)
     @clipart = Clipart.all.sort_by{ rand }
