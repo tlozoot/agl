8bbc18e4edb81e0cc850b11c9142f248ae72e7de
Jonathan Levine <jonathan@dhcp-0019874014-d7-ae.client.student.harvard.edu>
2010-09-29 11:59:36 -0400

    fixed static resource urls

--- app/models/clipart.rb
+++ app/models/clipart.rb
@@ -3,11 +3,11 @@ class Clipart < ActiveRecord::Base
   has_many :results
   
   def singular_file
-    "http://phonetics.fas.harvard.edu/AGL/clipart/#{name}1.jpg"
+    "http://phonetics.fas.harvard.edu/agl/clipart/#{name}1.jpg"
   end
   
   def plural_file
-    "http://phonetics.fas.harvard.edu/AGL/clipart/#{name}2.jpg"
+    "http://phonetics.fas.harvard.edu/agl/clipart/#{name}2.jpg"
   end
   
 end
