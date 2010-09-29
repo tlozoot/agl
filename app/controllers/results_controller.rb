13eda6e55fb754183a98afcae0059a34c4e9235d
Jonathan Levine <jonathan@jonathan-levines-macbook-pro.local>
2010-09-28 22:32:29 -0400

    worst security scheme ever

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,10 +1,26 @@
 class ResultsController < ApplicationController
   
-  before_filter :require_user
+  before_filter :require_user, :except => :index
   
   def index
     @participants = Participant.all
     @results = @participants.map(&:results).flatten
+    if current_user
+      respond_to do |format|
+        format.html
+        format.csv { @filename = 'results.csv'; render :layout => false }
+      end
+    elsif params[:id] == "super_secret_code"
+      respond_to do |format|
+        format.csv { @filename = 'results.csv'; render :layout => false }
+      end
+    else
+      flash[:message] = "Sorry, you need to log in or provide a valid access code."
+      redirect_to login_url
+    end
+  end
+  
+  def results
     respond_to do |format|
       format.html
       format.csv { @filename = 'results.csv'; render :layout => false }
